38e2ca2194d5c8a9e78f8e2719e9a6ebde329f39
IgorMedvedev <psydov81@gmail.com>
2024-11-05 19:29:09 +0200

    hw #65

--- pulse-jump-analyzer/src/main/java/telran/pulse/monitoring/App.java
+++ pulse-jump-analyzer/src/main/java/telran/pulse/monitoring/App.java
@@ -1,49 +1,109 @@
 package telran.pulse.monitoring;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.*;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
 import com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeValue;
 
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+
 public class App {
-	// FIXME move to DynamoDB Table
-	HashMap<String, Integer> lastValues = new HashMap<>();
+    static DynamoDbClient client = DynamoDbClient.builder().build();
+    static Logger logger = Logger.getLogger(App.class.getName());
+    static float factor;
 
-	public void handleRequest(DynamodbEvent event, Context context) {
-		event.getRecords().forEach(r -> {
+    static {
+        loggerSetUp();
+        factor = getFactor();
+    }
 
-			Map<String, AttributeValue> map = r.getDynamodb().getNewImage();
-			if (map == null) {
-				System.out.println("No new image found");
-			} else if (r.getEventName().equals("INSERT")) {
-				String patientId = map.get("patientId").getN();
-				Integer currentValue = Integer.parseInt(map.get("value").getN());
-				String timestamp = map.get("timestamp").getN();
-				Integer lastValue = lastValues.computeIfAbsent(patientId, k -> currentValue);
-				if (isJump(currentValue, lastValue)) {
-					jumpProcessing(patientId, currentValue, lastValue, timestamp);
-				}
-				lastValues.put(patientId, lastValue);
+    public void handleRequest(DynamodbEvent event, Context context) {
+        event.getRecords().forEach(r -> {
+            Map<String, AttributeValue> map = r.getDynamodb().getNewImage();
+            if (map == null) {
+                logger.warning("No new image found");
+            } else if ("INSERT".equals(r.getEventName())) {
+                String patientId = map.get("patientId").getN();
+                Integer currentValue = Integer.parseInt(map.get("value").getN());
+                String timestamp = map.get("timestamp").getN();
+                Integer lastValue = getLastPulseValue(patientId, currentValue);
 
-			} else {
-				System.out.println(r.getEventName());
-			}
+                if (isJump(currentValue, lastValue)) {
+                    recordJump(patientId, lastValue, currentValue, timestamp);
+                }
+                saveLastPulseValue(patientId, currentValue);
+            } else {
+                logger.warning("Unexpected event: " + r.getEventName());
+            }
+        });
+    }
 
-		});
-	}
+    private static void loggerSetUp() {
+        Level loggerLevel = getLoggerLevel();
+        LogManager.getLogManager().reset();
+        Handler handler = new ConsoleHandler();
+        logger.setLevel(loggerLevel);
+        handler.setLevel(Level.FINEST);
+        logger.addHandler(handler);
+    }
 
-	private void jumpProcessing(String patientId, Integer currentValue, Integer lastValue, String timestamp) {
-		// FIXME move to DynamoDB Table
-		System.out.printf("Jump: patientId is %s,lastValue is %d, currentValue is %d, timestamp is %s\n",
-				patientId, lastValue, currentValue, timestamp);
+    private static Level getLoggerLevel() {
+        String levelStr = System.getenv().getOrDefault("LOGGER_LEVEL", "INFO");
+        try {
+            return Level.parse(levelStr);
+        } catch (Exception e) {
+            return Level.INFO;
+        }
+    }
 
-	}
+    private static float getFactor() {
+        String factorStr = System.getenv().getOrDefault("FACTOR", "0.2");
+        try {
+            return Float.parseFloat(factorStr);
+        } catch (NumberFormatException e) {
+            return 0.2f;
+        }
+    }
 
-	private boolean isJump(Integer currentValue, Integer lastValue) {
-		// FIXME
-		float factor = 0.2f;
-		return (float) Math.abs(currentValue - lastValue) / lastValue > factor;
-	}
+    private boolean isJump(Integer currentValue, Integer lastValue) {
+        return (float) Math.abs(currentValue - lastValue) / lastValue > factor;
+    }
+
+    private Integer getLastPulseValue(String patientId, Integer defaultValue) {
+        GetItemRequest request = GetItemRequest.builder()
+            .tableName("pulse_last_value")
+            .key(Map.of("patientId", software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder().n(patientId).build()))
+            .build();
+
+        Map<String, software.amazon.awssdk.services.dynamodb.model.AttributeValue> item = client.getItem(request).item();
+        return item != null && item.containsKey("value") ? Integer.parseInt(item.get("value").n()) : defaultValue;
+    }
+
+    private void saveLastPulseValue(String patientId, Integer value) {
+        client.putItem(PutItemRequest.builder()
+            .tableName("pulse_last_value")
+            .item(Map.of(
+                "patientId", software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder().n(patientId).build(),
+                "value", software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder().n(value.toString()).build()
+            ))
+            .build());
+    }
+
+    private void recordJump(String patientId, Integer lastValue, Integer currentValue, String timestamp) {
+        client.putItem(PutItemRequest.builder()
+            .tableName("pulse_jump_values")
+            .item(Map.of(
+                "patientId", software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder().n(patientId).build(),
+                "PreviousValue", software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder().n(lastValue.toString()).build(),
+                "CurrentValue", software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder().n(currentValue.toString()).build(),
+                "timestamp", software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder().n(timestamp).build()
+            ))
+            .build());
+        logger.info(String.format("Jump recorded: patientId=%s, lastValue=%d, currentValue=%d, timestamp=%s",
+                patientId, lastValue, currentValue, timestamp));
+    }
 }
--- pulse-values-analyzer/src/main/java/telran/pulse/monitoring/App.java
+++ pulse-values-analyzer/src/main/java/telran/pulse/monitoring/App.java
@@ -1,89 +1,123 @@
 package telran.pulse.monitoring;
 
-import java.util.*;
-import java.util.logging.*;
+import java.util.Map;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
 import com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeValue;
-import software.amazon.awssdk.services.dynamodb.*;
+
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
 import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
-import software.amazon.awssdk.services.dynamodb.model.PutItemRequest.Builder;
-import static telran.pulse.monitoring.Constants.*;
+import static telran.pulse.monitoring.Constants.DEFAULT_LOGGER_LEVEL;
+import static telran.pulse.monitoring.Constants.LOGGER_LEVEL_ENV_VARIABLE;
+import static telran.pulse.monitoring.Constants.PATIENT_ID_ATTRIBUTE;
+import static telran.pulse.monitoring.Constants.TIMESTAMP_ATTRIBUTE;
+import static telran.pulse.monitoring.Constants.VALUE_ATTRIBUTE;
 
 public class App {
 
-	static DynamoDbClient client = DynamoDbClient.builder().build();
-	static Builder request;
-	static Logger logger = Logger.getLogger("pulse-value-analyzer");
-	static {
-		loggerSetUp();
-		
-	}
+    static DynamoDbClient client = DynamoDbClient.builder().build();
+    static Logger logger = Logger.getLogger("pulse-value-analyzer");
+    static float factor;
 
-	public void handleRequest(DynamodbEvent event, Context context) {
-		request = PutItemRequest.builder().tableName(ABNORMAL_VALUES_TABLE_NAME);
-		event.getRecords().forEach(r -> {
-			Map<String, AttributeValue> map = r.getDynamodb().getNewImage();
-			if (map == null) {
-				logger.warning("No new image found");
-			} else if (r.getEventName().equals("INSERT")) {
-				processPulseValue(map);
-			} else {
-				logger.warning(String.format("The event isn't INSERT but %s", r.getEventName()));
-			}
+    static {
+        loggerSetUp();
+        factor = getFactor();
+    }
 
-		});
-	}
+    public void handleRequest(DynamodbEvent event, Context context) {
+        event.getRecords().forEach(r -> {
+            Map<String, AttributeValue> map = r.getDynamodb().getNewImage();
+            if (map == null) {
+                logger.warning("No new image found");
+            } else if ("INSERT".equals(r.getEventName())) {
+                processPulseValue(map);
+            } else {
+                logger.warning(String.format("The event isn't INSERT but %s", r.getEventName()));
+            }
+        });
+    }
 
-	private static void loggerSetUp() {
-		Level loggerLevel = getLoggerLevel();
-		LogManager.getLogManager().reset();
-		Handler handler = new ConsoleHandler();
-		logger.setLevel(loggerLevel);
-		handler.setLevel(Level.FINEST);
-		logger.addHandler(handler);
-	}
+    private static void loggerSetUp() {
+        Level loggerLevel = getLoggerLevel();
+        LogManager.getLogManager().reset();
+        Handler handler = new ConsoleHandler();
+        logger.setLevel(loggerLevel);
+        handler.setLevel(Level.FINEST);
+        logger.addHandler(handler);
+    }
 
-	private static Level getLoggerLevel() {
-		String levelStr = System.getenv()
-		.getOrDefault(LOGGER_LEVEL_ENV_VARIABLE, DEFAULT_LOGGER_LEVEL);
-		Level res = null;
-		try {
-			res = Level.parse(levelStr);
-		} catch (Exception e) {
-			res = Level.parse(DEFAULT_LOGGER_LEVEL);
-		}
-		return res;
-	}
+    private static Level getLoggerLevel() {
+        String levelStr = System.getenv().getOrDefault(LOGGER_LEVEL_ENV_VARIABLE, DEFAULT_LOGGER_LEVEL);
+        try {
+            return Level.parse(levelStr);
+        } catch (Exception e) {
+            return Level.parse(DEFAULT_LOGGER_LEVEL);
+        }
+    }
 
-	private void processPulseValue(Map<String, AttributeValue> map) {
-		int value = Integer.parseInt(map.get(VALUE_ATTRIBUTE).getN());
-		logger.finer(getLogMessage(map));
-		if (value > MAX_THRESHOLD_PULSE_VALUE || value < MIN_THRESHOLD_PULSE_VALUE) {
-			processAbnormalPulseValue(map);
-		}
-	}
+    private static float getFactor() {
+        String factorStr = System.getenv().getOrDefault("FACTOR", "0.2");
+        try {
+            return Float.parseFloat(factorStr);
+        } catch (NumberFormatException e) {
+            return 0.2f;
+        }
+    }
 
-	private void processAbnormalPulseValue(Map<String, AttributeValue> map) {
-		logger.info(getLogMessage(map));
-		client.putItem(request.item(getPutItemMap(map)).build());
-	}
+    private void processPulseValue(Map<String, AttributeValue> map) {
+        int value = Integer.parseInt(map.get(VALUE_ATTRIBUTE).getN());
+        String patientId = map.get(PATIENT_ID_ATTRIBUTE).getN();
+        int lastValue = getLastPulseValue(patientId);
+        logger.finer(getLogMessage(map));
+        if (isJump(value, lastValue)) {
+            recordJump(patientId, lastValue, value, map.get(TIMESTAMP_ATTRIBUTE).getN());
+        }
+        saveLastPulseValue(patientId, value);
+    }
 
-	private Map<String, software.amazon.awssdk.services.dynamodb.model.AttributeValue> getPutItemMap(
-			Map<String, AttributeValue> map) {
-		Map<String, software.amazon.awssdk.services.dynamodb.model.AttributeValue> res = new HashMap<>();
-		res.put(PATIENT_ID_ATTRIBUTE, software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder()
-				.n(map.get(PATIENT_ID_ATTRIBUTE).getN()).build());
-		res.put(TIMESTAMP_ATTRIBUTE, software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder()
-				.n(map.get(TIMESTAMP_ATTRIBUTE).getN()).build());
-		res.put(VALUE_ATTRIBUTE, software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder()
-				.n(map.get(VALUE_ATTRIBUTE).getN()).build());
-		return res;
-	}
+    private boolean isJump(int currentValue, int lastValue) {
+        return (float) Math.abs(currentValue - lastValue) / lastValue > factor;
+    }
 
-	private String getLogMessage(Map<String, AttributeValue> map) {
-		return String.format("patientId: %s, value: %s", map.get(PATIENT_ID_ATTRIBUTE).getN(),
-				map.get(VALUE_ATTRIBUTE).getN());
-	}
+    private String getLogMessage(Map<String, AttributeValue> map) {
+        return String.format("patientId: %s, value: %s", map.get(PATIENT_ID_ATTRIBUTE).getN(), map.get(VALUE_ATTRIBUTE).getN());
+    }
+
+    private int getLastPulseValue(String patientId) {
+        GetItemRequest request = GetItemRequest.builder()
+            .tableName("pulse_last_value")
+            .key(Map.of(PATIENT_ID_ATTRIBUTE, software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder().n(patientId).build()))
+            .build();
+        Map<String, software.amazon.awssdk.services.dynamodb.model.AttributeValue> item = client.getItem(request).item();
+        return item != null && item.containsKey(VALUE_ATTRIBUTE) ? Integer.parseInt(item.get(VALUE_ATTRIBUTE).n()) : 0;
+    }
+
+    private void saveLastPulseValue(String patientId, int value) {
+        client.putItem(PutItemRequest.builder()
+            .tableName("pulse_last_value")
+            .item(Map.of(
+                PATIENT_ID_ATTRIBUTE, software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder().n(patientId).build(),
+                VALUE_ATTRIBUTE, software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder().n(String.valueOf(value)).build()
+            ))
+            .build());
+    }
+
+    private void recordJump(String patientId, int lastValue, int currentValue, String timestamp) {
+        client.putItem(PutItemRequest.builder()
+            .tableName("pulse_jump_values")
+            .item(Map.of(
+                PATIENT_ID_ATTRIBUTE, software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder().n(patientId).build(),
+                "PreviousValue", software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder().n(String.valueOf(lastValue)).build(),
+                "CurrentValue", software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder().n(String.valueOf(currentValue)).build(),
+                TIMESTAMP_ATTRIBUTE, software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder().n(timestamp).build()
+            ))
+            .build());
+    }
 }
